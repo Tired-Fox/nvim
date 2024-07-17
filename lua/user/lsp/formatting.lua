78f74d1029a577add3bb0f4136579af0489617b8
Zachary Boehm <zboehm@menard-inc.com>
2024-07-17 10:09:52 -0500

    [feat] html formatting, use java command shortening for debugging

--- lua/user/lsp/formatting.lua
+++ lua/user/lsp/formatting.lua
@@ -2,6 +2,7 @@
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
+		html = { "prettierd", "prettier" },
 	},
 })
 
--- lua/user/lsp/jdtls.lua
+++ lua/user/lsp/jdtls.lua
@@ -24,7 +24,7 @@ function JavaRuntimes(opts)
 					}
 				end,
 				fn = function()
-					local runtimes = require("jdtls")._complete_set_runtime()
+					local runtimes = jdtls._complete_set_runtime()
 					if type(runtimes) ~= "table" then
 						local _runtimes = {}
 						for runtime in runtimes:gmatch("[^\r\n]+") do
@@ -41,7 +41,7 @@ function JavaRuntimes(opts)
 					actions.close(prompt_bufnr)
 					local selection = action_state.get_selected_entry()
 					vim.notify("Setting jdtls java runtime to " .. selection.value, vim.log.levels.INFO)
-					require("jdtls").set_runtime(selection.value)
+					jdtls.set_runtime(selection.value)
 				end)
 				return true
 			end,
@@ -52,14 +52,27 @@ end
 -- to execute the function
 -- JavaRuntimes(require("telescope.themes").get_dropdown({}))
 
+local overrides = {
+	-- This puts the java arguments in an arguments file: Only for java versions 9+
+	-- shortenCommandLine = "argfile",
+	-- This creates a jar manifest that contains the classpath and java arguments works with most if not all  java versions
+	shortenCommandLine = "jarmanifest",
+}
+
 vim.api.nvim_create_autocmd("LspAttach", {
 	pattern = "*.java",
 	callback = function()
 		vim.keymap.set("n", "<space>tm", function()
-			jdtls.test_nearest_method()
+			vim.print("Testing nearest test method")
+			jdtls.test_nearest_method({
+				config_overrides = overrides,
+			})
 		end, { buffer = 0, desc = "Test nearest method" })
 		vim.keymap.set("n", "<space>tc", function()
-			jdtls.test_class()
+			vim.print("Testing entire test class")
+			jdtls.test_class({
+				config_overrides = overrides,
+			})
 		end, { buffer = 0, desc = "Test class" })
 	end,
 })
